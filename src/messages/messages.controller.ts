2e4920abd13e78b1a8960c112210b1d104f98902
Nishant <sharmanishant6500@gmail.com>
2025-01-28 02:49:51 +0530

    messages app

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { createMessageDto } from './dtos/create-message.dto';
+
+@Controller('messages')
+export class MessagesController {
+    @Get()
+    listMessages(){}
+
+    @Post()
+    createMessage(@Body() body: createMessageDto){
+        console.log(body);
+    }
+    
+    @Get('/:id')
+    getMessage(@Param('id') id: string){ 
+        console.log(id);
+    }
+}
---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,15 @@
+
+
+export class MessagesRepository {
+    async findOne(id:string) {
+        
+    }
+
+    async findAll() {
+        
+    }
+    
+    async create(message:string) {
+        
+    }
+}
