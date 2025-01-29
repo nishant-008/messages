a427db08592a03459a09797a2dfbf783ea2050a2
Nishant <sharmanishant6500@gmail.com>
2025-01-29 13:47:42 +0530

    added DI in code

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,28 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { createMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    
+        constructor(public messagesService: MessagesService) {}
     @Get()
-    listMessages(){}
+    listMessages(){
+        return this.messagesService.findAll();
+    }
 
     @Post()
     createMessage(@Body() body: createMessageDto){
-        console.log(body);
+        this.messagesService.create(body.content);
+        return 'Message created successfully!';
     }
     
     @Get('/:id')
-    getMessage(@Param('id') id: string){ 
-        console.log(id);
+    async getMessage(@Param('id') id: string){ 
+        const message = await this.messagesService.findOne(id);
+
+        if (!message) {
+            throw new NotFoundException(`Message with id ${id} not found.`);
+        }
     }
 }
--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,15 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { readFile, writeFile } from "fs/promises";
 
-
+@Injectable()
 export class MessagesRepository {
     async findOne(id:string) {
-        
+        const constents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(constents);
+
+        return messages[id];
     }
 
     async findAll() {
-        
+        const constents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(constents);
+
+        return messages;
     }
     
-    async create(message:string) {
-        
+    async create(content:string) {
+        const constents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(constents);
+
+        const id = Math.floor(Math.random() * 999);
+
+        messages[id] = {id, content};
+
+        await writeFile( 'messages.json', JSON.stringify(messages));
     }
 }
